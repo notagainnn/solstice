5dc7cae0b6ed0d887ef95358e1dac07bf87fb224
notagainnn <savagedialects@gmail.com>
2022-03-28 11:33:28 -0700

    Solstice Salon

---
+++ js.js
@@ -0,0 +1,6 @@
+//modal
+var myModal = document.getElementById('exampleModal')
+var myInput = document.getElementById('myInput')
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
